9d1d903319b5cce63fe5cf1e15bc4b2f51dcb0ae
Muhammad Mahad Tariq <mahad.tariq@emumba.com>
2021-08-24 16:58:28 +0500

    Applying formulas to calculate result

--- app/src/main/java/com/example/travelexpenses/HomeFragment.java
+++ app/src/main/java/com/example/travelexpenses/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.travelexpenses;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 public class HomeFragment extends Fragment {
     @Nullable
@@ -25,48 +27,90 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-        final EditText et_kg = view.findViewById(R.id.et_kg);
+        final EditText et_car_avg = view.findViewById(R.id.et_car_avg);
 
-        final EditText et_feet = view.findViewById(R.id.et_feet);
+        final EditText et_days = view.findViewById(R.id.et_days);
 
-        final EditText et_inches = view.findViewById(R.id.et_inches);
+        final EditText et_distance_kms = view.findViewById(R.id.et_distance_kms);
+
+        final EditText et_num_of_people = view.findViewById(R.id.et_num_of_people);
+
+        final EditText et_petrol_per_ltr = view.findViewById(R.id.et_petrol_per_ltr);
+
         final TextView tv_res = view.findViewById(R.id.tv_res);
 
-        final Button bt_convert = view.findViewById(R.id.bt_convert);
+        final Button bt_calculate = view.findViewById(R.id.bt_calculate);
 
-        bt_convert.setOnClickListener(new View.OnClickListener() {
+        final SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        bt_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String kgs = et_kg.getText().toString();
-                String ft = et_feet.getText().toString();
-                String inches = et_inches.getText().toString();
+                String carAvg = et_car_avg.getText().toString();
+                String days = et_days.getText().toString();
+                String distance = et_distance_kms.getText().toString();
+                String noOfPeople = et_num_of_people.getText().toString();
+                String petrolPerLitre = et_petrol_per_ltr.getText().toString();
 
-                if (kgs.length() == 0 || "".equals(kgs)) {
-                    Toast.makeText(getActivity(), "Please enter some value in kgs field.", Toast.LENGTH_SHORT).show();
-                } else if (ft.length() == 0 || "".equals(ft)) {
-                    Toast.makeText(getActivity(), "Please enter some value in feet field.", Toast.LENGTH_SHORT).show();
-                } else if (inches.length() == 0 || "".equals(inches)) {
-                    Toast.makeText(getActivity(), "Please enter some value in inches field.", Toast.LENGTH_SHORT).show();
-                } else {
+//                if (kgs.length() == 0 || "".equals(kgs)) {
+//                    Toast.makeText(getActivity(), "Please enter some value in kgs field.", Toast.LENGTH_SHORT).show();
+//                } else if (ft.length() == 0 || "".equals(ft)) {
+//                    Toast.makeText(getActivity(), "Please enter some value in feet field.", Toast.LENGTH_SHORT).show();
+//                } else if (inches.length() == 0 || "".equals(inches)) {
+//                    Toast.makeText(getActivity(), "Please enter some value in inches field.", Toast.LENGTH_SHORT).show();
+//                } else {
 
-                    double feet = Double.parseDouble(ft);
-                    double inch = Double.parseDouble(inches);
-                    double meters = (feet * 0.3048) + (inch * 0.0254);
-                    double kg = Double.parseDouble(kgs);
-                    double bmi = kg / (meters * meters);
-                    String bmi_string =String.format("%.1f", bmi);
+                double carAverage = Double.parseDouble(carAvg);
+                double numOfDays = Double.parseDouble(days);
+                double dist = Double.parseDouble(distance);
+                double numOfPeople = Double.parseDouble(noOfPeople);
+                double petrolPerLtr = Double.parseDouble(petrolPerLitre);
 
-                    if  (bmi<18.5){
-                        tv_res.setText("BMI = " + bmi_string + " Underweight");
-                    }else if(bmi<24.9){
-                        tv_res.setText("BMI = " + bmi_string + " Normal");
-                    }else if(bmi<29.9){
-                        tv_res.setText("BMI = " + bmi_string + " Overweight");
-                    }else {
-                        tv_res.setText("BMI = " + bmi_string + " Obese");
-                    }
+                String tire_usage_life = sharedPreferences.getString("tire_usage_life", "0");
+                String cost_tires_set = sharedPreferences.getString("cost_tires_set", "0");
+                String mobil_oil_usage_life = sharedPreferences.getString("mobil_oil_usage_life", "0");
+                String mobil_oil_cost = sharedPreferences.getString("mobil_oil_cost", "0");
+                String battery_usage_life = sharedPreferences.getString("battery_usage_life", "0");
+                String battery_cost = sharedPreferences.getString("battery_cost", "0");
+                String suspension_usage_life = sharedPreferences.getString("suspension_usage_life", "0");
+                String suspension_cost = sharedPreferences.getString("suspension_cost", "0");
 
-                }
+                double tire_usage_life_val = Double.parseDouble("tire_usage_life");
+                double cost_tires_set_val = Double.parseDouble("cost_tires_set");
+                double mobil_oil_usage_life_val = Double.parseDouble("mobil_oil_usage_life");
+                double mobil_oil_cost_val = Double.parseDouble("mobil_oil_cost");
+                double battery_usage_life_val = Double.parseDouble("battery_usage_life");
+                double battery_cost_val = Double.parseDouble("battery_cost");
+                double suspension_usage_life_val = Double.parseDouble("suspension_usage_life");
+                double suspension_cost_val = Double.parseDouble("suspension_cost");
+
+                double tirePerKmCost = cost_tires_set_val / tire_usage_life_val;
+                double mobilOilPerKmCost = mobil_oil_cost_val / mobil_oil_usage_life_val;
+                double batteryPerDayCost = battery_cost_val / (battery_usage_life_val * 365);
+                double suspensionPerDayCost = suspension_cost_val / (suspension_usage_life_val * 365);
+
+                double tireWear = tirePerKmCost * dist;
+                double batteryWear = batteryPerDayCost * numOfDays;
+                double suspensionWear = suspensionPerDayCost * numOfDays;
+                double mobileOilUse = mobilOilPerKmCost * dist;
+
+//                double meters = (feet * 0.3048) + (inch * 0.0254);
+//                double kg = Double.parseDouble(kgs);
+//                double bmi = kg / (meters * meters);
+//                String bmi_string = String.format("%.1f", bmi);
+//
+//                if (bmi < 18.5) {
+//                    tv_res.setText("BMI = " + bmi_string + " Underweight");
+//                } else if (bmi < 24.9) {
+//                    tv_res.setText("BMI = " + bmi_string + " Normal");
+//                } else if (bmi < 29.9) {
+//                    tv_res.setText("BMI = " + bmi_string + " Overweight");
+//                } else {
+//                    tv_res.setText("BMI = " + bmi_string + " Obese");
+//                }
+
+//                }
             }
         });
     }
