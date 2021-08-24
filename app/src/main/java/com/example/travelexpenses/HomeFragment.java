a849390bc7ae7d57c94bad7bd4ede4aa25656a91
Muhammad Mahad Tariq <mahad.tariq@emumba.com>
2021-08-24 17:50:02 +0500

    Added formulas. Scroll view not working in home screen

--- app/src/main/java/com/example/travelexpenses/HomeFragment.java
+++ app/src/main/java/com/example/travelexpenses/HomeFragment.java
@@ -67,23 +67,23 @@ public class HomeFragment extends Fragment {
                 double numOfPeople = Double.parseDouble(noOfPeople);
                 double petrolPerLtr = Double.parseDouble(petrolPerLitre);
 
-                String tire_usage_life = sharedPreferences.getString("tire_usage_life", "0");
-                String cost_tires_set = sharedPreferences.getString("cost_tires_set", "0");
-                String mobil_oil_usage_life = sharedPreferences.getString("mobil_oil_usage_life", "0");
-                String mobil_oil_cost = sharedPreferences.getString("mobil_oil_cost", "0");
-                String battery_usage_life = sharedPreferences.getString("battery_usage_life", "0");
-                String battery_cost = sharedPreferences.getString("battery_cost", "0");
-                String suspension_usage_life = sharedPreferences.getString("suspension_usage_life", "0");
-                String suspension_cost = sharedPreferences.getString("suspension_cost", "0");
+                String tire_usage_life = sharedPreferences.getString("tire_usage_life", "");
+                String cost_tires_set = sharedPreferences.getString("cost_tires_set", "");
+                String mobil_oil_usage_life = sharedPreferences.getString("mobil_oil_usage_life", "");
+                String mobil_oil_cost = sharedPreferences.getString("mobil_oil_cost", "");
+                String battery_usage_life = sharedPreferences.getString("battery_usage_life", "");
+                String battery_cost = sharedPreferences.getString("battery_cost", "");
+                String suspension_usage_life = sharedPreferences.getString("suspension_usage_life", "");
+                String suspension_cost = sharedPreferences.getString("suspension_cost", "");
 
-                double tire_usage_life_val = Double.parseDouble("tire_usage_life");
-                double cost_tires_set_val = Double.parseDouble("cost_tires_set");
-                double mobil_oil_usage_life_val = Double.parseDouble("mobil_oil_usage_life");
-                double mobil_oil_cost_val = Double.parseDouble("mobil_oil_cost");
-                double battery_usage_life_val = Double.parseDouble("battery_usage_life");
-                double battery_cost_val = Double.parseDouble("battery_cost");
-                double suspension_usage_life_val = Double.parseDouble("suspension_usage_life");
-                double suspension_cost_val = Double.parseDouble("suspension_cost");
+                double tire_usage_life_val = Double.parseDouble(tire_usage_life);
+                double cost_tires_set_val = Double.parseDouble(cost_tires_set);
+                double mobil_oil_usage_life_val = Double.parseDouble(mobil_oil_usage_life);
+                double mobil_oil_cost_val = Double.parseDouble(mobil_oil_cost);
+                double battery_usage_life_val = Double.parseDouble(battery_usage_life);
+                double battery_cost_val = Double.parseDouble(battery_cost);
+                double suspension_usage_life_val = Double.parseDouble(suspension_usage_life);
+                double suspension_cost_val = Double.parseDouble(suspension_cost);
 
                 double tirePerKmCost = cost_tires_set_val / tire_usage_life_val;
                 double mobilOilPerKmCost = mobil_oil_cost_val / mobil_oil_usage_life_val;
@@ -95,6 +95,13 @@ public class HomeFragment extends Fragment {
                 double suspensionWear = suspensionPerDayCost * numOfDays;
                 double mobileOilUse = mobilOilPerKmCost * dist;
 
+                double costPerPerson = ((dist / carAverage) * petrolPerLtr) / numOfPeople;
+                double wearPerPerson = (tireWear + batteryWear + suspensionWear + mobileOilUse) / numOfPeople;
+
+                double result = costPerPerson + wearPerPerson;
+                String result_string = String.format("%.1f", result);
+
+                tv_res.setText("Net cost per person = Rs. " + result_string );
 //                double meters = (feet * 0.3048) + (inch * 0.0254);
 //                double kg = Double.parseDouble(kgs);
 //                double bmi = kg / (meters * meters);
